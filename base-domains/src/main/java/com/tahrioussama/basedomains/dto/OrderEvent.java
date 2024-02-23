24c24dd23af8fe6191e17329b08c847fe3f17287
oussama-tahri <tahrioussama.ot@gmail.com>
2024-02-23 20:08:02 +0100

    kafka for real time streaming data of order,stock and email

---
+++ base-domains/src/main/java/com/tahrioussama/basedomains/dto/OrderEvent.java
@@ -0,0 +1,13 @@
+package com.tahrioussama.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+}
